46de91508e91dc4ba2070512251c3cee51a26bad
+ <+>
2023-02-18 11:39:51 -0700

    sending data with fetch completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const newUser = {
+    name: userName,
+    email: userEmail
+  }
+  const configData = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "accept": "application/json",
+    },
+    body: JSON.stringify(newUser)
+  }
+  return fetch("http://localhost:3000/users", configData)
+    .then(res => res.json())
+    .then(function (jsonString) {
+      document.body.append(jsonString.id)
+    })
+    .catch(function (error) {
+      document.body.append(error.message)
+    })
+
+
+}
+
+submitData()
